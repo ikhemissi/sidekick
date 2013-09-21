cb8cdfa49c0b408810e28dc334cf85d75310bbe3
Iheb KHEMISSI <iheb.khemissi+github@gmail.com>
2013-09-21 09:16:20 -0400

    adding a new dependency : Cargo

--- project/Build.scala
+++ project/Build.scala
@@ -10,8 +10,12 @@ object ApplicationBuild extends Build {
       javaCore,
       javaJdbc,
       javaEbean,
+      // Database
+      "postgresql" % "postgresql" % "9.1-901-1.jdbc4",
+      // IMB iSeries
       "net.sf.jt400" % "jt400" % "6.7",
-      "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
+      // JEE servers management using Cargo
+      "org.codehaus.cargo" % "cargo-core-uberjar" % "1.4.4"
     )
 
     val main = play.Project(appName, appVersion, appDependencies).settings(
